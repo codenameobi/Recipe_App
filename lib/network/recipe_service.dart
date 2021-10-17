fe23f7b7e9eb4b0a6c9ffb7c5faf85fdb0ca098f
Obiora Nwude <44972479+datcodeboy@users.noreply.github.com>
2021-10-17 16:55:07 +0100

    generated recipe_service.chopper

--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -1,6 +1,8 @@
 import 'package:chopper/chopper.dart';
 import 'recipe_model.dart';
 import 'model_response.dart';
+import 'model_converter.dart';
+part 'recipe_service.chopper.dart';
 
 const String apiKey = '9c906bb633df0a77c5525cb978ed314d';
 const String apiId = 'c3c51441';
@@ -10,14 +12,26 @@ const String apiUrl = 'https://api.edamam.com';
 abstract class RecipeService extends ChopperService {
   @Get(path: 'search')
   Future<Response<Result<APIRecipeQuery>>> queryRecipes(
-      @Query('q') String query, @Query('from') int from,
-      @Query('to') int to);
+      @Query('q') String query, @Query('from') int from, @Query('to') int to);
+
+  static RecipeService create() {
+    final client = ChopperClient(
+      baseUrl: apiUrl,
+      interceptors: [_addQuery, HttpLoggingInterceptor()],
+      converter: ModelConverter(),
+      errorConverter: const JsonConverter(),
+      services: [
+        _$RecipeService(),
+      ],
+    );
+    return _$RecipeService(client);
+  }
 }
 
 Request _addQuery(Request req) {
   final params = Map<String, dynamic>.from(req.parameters);
-  params['c3c51441'] = apiId;
-  params['9c906bb633df0a77c5525cb978ed314d'] = apiKey;
+  params['app_id'] = apiId;
+  params['app_key'] = apiKey;
 
   return req.copyWith(parameters: params);
 }
