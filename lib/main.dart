825add4d6c8141a819663b23965eed2058d5c65a
Obiora Nwude <44972479+datcodeboy@users.noreply.github.com>
2021-10-17 16:56:42 +0100

    setting up logging requests and responses

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,13 @@ import 'ui/main_screen.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+  _setupLogging();
+}
+
+void _setupLogging() {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((rec) {
+    print('${rec.level.name}: ${rec.time}: ${rec.message}');   });
 }
 
 class MyApp extends StatelessWidget {
