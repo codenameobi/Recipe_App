f588c82804bfff73d5e361ddd7a078f429e203f0
Obiora Nwude <44972479+datcodeboy@users.noreply.github.com>
2021-10-17 16:14:01 +0100

    setting up chopper interceptors to decorate request

--- lib/network/recipe_service.dart
+++ lib/network/recipe_service.dart
@@ -13,3 +13,11 @@ abstract class RecipeService extends ChopperService {
       @Query('q') String query, @Query('from') int from,
       @Query('to') int to);
 }
+
+Request _addQuery(Request req) {
+  final params = Map<String, dynamic>.from(req.parameters);
+  params['c3c51441'] = apiId;
+  params['9c906bb633df0a77c5525cb978ed314d'] = apiKey;
+
+  return req.copyWith(parameters: params);
+}
